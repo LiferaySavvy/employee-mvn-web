4e72d003d412e4ffd7738dc8c06420dbf588e6be
iamliferaysavvy <iamliferaysavvy@gmail.com>
2020-03-06 21:09:17 -0500

    first commit

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 # employee-mvn-web
+
+## Installation
+
+git clone https://github.com/iamliferaysavvy/employee-mvn-web.git
+mvn clean install
+mvn bundle-support:deploy
+
+## Usage
+<link-here>
--- src/main/java/com/liferaysavvy/mvn/employee/portlet/EmployeePortlet.java
+++ src/main/java/com/liferaysavvy/mvn/employee/portlet/EmployeePortlet.java
@@ -2,10 +2,24 @@ package com.liferaysavvy.mvn.employee.portlet;
 
 import com.liferaysavvy.mvn.employee.constants.EmployeePortletKeys;
 
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Iterator;
+
 import com.liferay.portal.kernel.portlet.bridges.mvc.MVCPortlet;
 
 import javax.portlet.Portlet;
+import javax.portlet.PortletException;
+import javax.portlet.RenderRequest;
+import javax.portlet.RenderResponse;
 
+import org.apache.poi.ss.usermodel.Cell;
+import org.apache.poi.ss.usermodel.CellType;
+import org.apache.poi.ss.usermodel.Row;
+import org.apache.poi.ss.usermodel.Sheet;
+import org.apache.poi.ss.usermodel.Workbook;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 import org.osgi.service.component.annotations.Component;
 
 /**
@@ -27,4 +41,43 @@ import org.osgi.service.component.annotations.Component;
 	service = Portlet.class
 )
 public class EmployeePortlet extends MVCPortlet {
+	@Override
+	public void render(RenderRequest request, RenderResponse response)
+		throws IOException, PortletException {
+		readXLSheet();
+		super.render(request, response);
+	}
+	
+	public void readXLSheet() {
+
+        try {
+        	InputStream is = this.getClass().getResourceAsStream("/content/liferaysavvy.xlsx");
+            Workbook workbook = new XSSFWorkbook(is);
+            Sheet datatypeSheet = workbook.getSheetAt(0);
+            Iterator<Row> iterator = datatypeSheet.iterator();
+
+            while (iterator.hasNext()) {
+
+                Row currentRow = iterator.next();
+                Iterator<Cell> cellIterator = currentRow.iterator();
+
+                while (cellIterator.hasNext()) {
+
+                    Cell currentCell = cellIterator.next();
+                    if (currentCell.getCellType() == CellType.STRING) {
+                        System.out.print(currentCell.getStringCellValue() + "--");
+                    } else if (currentCell.getCellType() == CellType.NUMERIC) {
+                        System.out.print(currentCell.getNumericCellValue() + "--");
+                    }
+
+                }
+                System.out.println();
+            }
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }
 }
