631161dff9b64c12530e5b7fbf6509cbc04b1095
iamliferaysavvy <iamliferaysavvy@gmail.com>
2020-03-06 20:54:52 -0500

    first commit

---
+++ src/main/java/com/liferaysavvy/mvn/employee/constants/EmployeePortletKeys.java
@@ -0,0 +1,11 @@
+package com.liferaysavvy.mvn.employee.constants;
+
+/**
+ * @author meera
+ */
+public class EmployeePortletKeys {
+
+	public static final String EMPLOYEE =
+		"com_liferaysavvy_mvn_employee_EmployeePortlet";
+
+}
